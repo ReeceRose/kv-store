2367ab9cb9559fa750b08e9ce16c52f1756dbc6c
Reece Rose <reecerose18@gmail.com>
2021-06-10 19:56:45 -0400

    Now writing a key value pair to file

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let mut arguments = std::env::args().skip(1);
+    let key = arguments.next().unwrap(); //unwrap is not recommended.
+    let value = arguments.next().unwrap();
+    println!("The key is '{}' and the value is '{}'", key, value);
+
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents).unwrap();
+}
